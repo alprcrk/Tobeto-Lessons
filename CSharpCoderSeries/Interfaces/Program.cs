740963399c9c01dbaa05a52503d3e24856a8719b
Alper Cirak <alprcrk@hotmail.com>
2023-12-10 06:31:46 +0300

    Interfaces added

---
+++ CSharpCoderSeries/Interfaces/Program.cs
@@ -0,0 +1,70 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //IPersonManager customerManager = new CustomerManager();
+        //customerManager.Add();
+
+        //IPersonManager employeeManager = new EmployeeManager();
+        //employeeManager.Add();
+
+        ProjecetManager projecetManager = new ProjecetManager();
+        projecetManager.Add(new InternManager());
+    }
+
+
+    interface IPersonManager
+    {
+        void Add();
+        void Update();
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel Güncellendi");
+        }
+        }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer Güncellendi");
+        }
+    }
+    class ProjecetManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            personManager.Add();
+        }
+
+        public void Add(EmployeeManager employeeManager)
+        {
+            employeeManager.Add();
+        }
+    }
+}
